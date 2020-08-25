1b999bf65452289a854741909c02e9d4b868d123
dawoodiddris <dawoodiddris@gmail.com>
2020-08-25 14:17:29 +0000

    Ready to deploy

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/task')
+def task():
+    return render_template('createTask.html')
+
+
